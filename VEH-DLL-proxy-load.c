4beb6c522290c349dc8e98567a89f1e4249e17e2
kleiton0x00 <37262788+kleiton0x00@users.noreply.github.com>
2024-01-20 21:05:10 +0100

    Create VEH-DLL-proxy-load.c

---
+++ VEH-DLL-proxy-load.c
@@ -0,0 +1,58 @@
+#include <Windows.h>
+#include <stdio.h>
+
+// Module to load, change to your liking
+static LPCSTR moduleName = "user32.dll";
+
+// Exception handler function
+LONG WINAPI VectoredExceptionHandler(PEXCEPTION_POINTERS ExceptionInfo)
+{
+    // Check for STATUS_GUARD_PAGE_VIOLATION
+    if (ExceptionInfo->ExceptionRecord->ExceptionCode == STATUS_GUARD_PAGE_VIOLATION)
+    {
+        // Get the address of "LoadLibraryA"
+        FARPROC loadLibraryAddr = GetProcAddress(GetModuleHandleA("kernel32.dll"), "LoadLibraryA");
+
+        // Set RAX register to the address of "LoadLibraryA"
+        ExceptionInfo->ContextRecord->Rip = (DWORD64)loadLibraryAddr;
+        // RCX hold the argument (library name)
+        ExceptionInfo->ContextRecord->Rcx = (DWORD64)moduleName;
+
+        // Resume execution
+        return EXCEPTION_CONTINUE_EXECUTION; //Continue to next instruction
+    }
+
+    // Continue searching for other exception handlers
+    return EXCEPTION_CONTINUE_SEARCH;
+}
+
+HMODULE proxiedLoadLibraryA(LPCSTR libName)
+{
+    // Just something to get its address to trigger the VEH
+    void(WINAPI* o_Sleep)(DWORD dwMilliseconds) = Sleep;
+
+    // Install the Vectored Exception Handler
+    PVOID handler = AddVectoredExceptionHandler(1, VectoredExceptionHandler);
+    if (!handler)
+    {
+        fprintf(stderr, "Failed to install Vectored Exception Handler\n");
+        return nullptr;
+    }
+
+    // Triggering the VEH by setting page to PAGE_GUARD
+    DWORD oldProtection = 0;
+    VirtualProtect((LPVOID)Sleep, 1, PAGE_EXECUTE_READ | PAGE_GUARD, &oldProtection);
+
+    // The module got loaded, so retrieve its base address
+    HMODULE addr = GetModuleHandleA(moduleName);
+
+    // Remove the Vectored Exception Handler
+    RemoveVectoredExceptionHandler(handler);
+
+    return addr;
+}
+
+int main() {
+    HMODULE user32 = proxiedLoadLibraryA(moduleName);
+    printf("%s Address: %p\n", moduleName, user32);
+}
